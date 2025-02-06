27bbd3c3f1ab629e753d8f1445d2db26a13bd256
Vivek Joshi <37882929+vivekjoshi556@users.noreply.github.com>
2025-02-06 14:40:53 +0000

    feat: initial commands

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,13 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
-
   std::string input;
-  std::getline(std::cin, input);
+  while(true) {
+    std::cout << "$ ";
+    std::getline(std::cin, input);
+    std::cout << input << ": command not found" << std::endl;
+    if(input == "exit 0") {
+      return 0;
+    }
+  }
 }
